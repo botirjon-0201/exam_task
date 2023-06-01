14d73d58eaf087a3c266744c29b03a3b6454b942
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-01 16:02:06 +0500

    main.ts file configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { getSwaggerConfig } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe());
+
+    const config = getSwaggerConfig;
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/exam-task', app, document);
+
+    const PORT = process.env.PORT || 5555;
+    await app.listen(PORT);
+    console.log(`Server is working on port ${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
 }
 bootstrap();
