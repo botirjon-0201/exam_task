d0bcc7229908e22d5f8ff36dc2bbaa97679af5bd
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-01 17:57:02 +0500

    users, organizations, projects, tasks, auth modules imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { getFilePathConfig } from './config/filepath.config';
 import { getSequelizeConfig } from './config/sequelize.config';
+import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +17,11 @@ import { getSequelizeConfig } from './config/sequelize.config';
       useFactory: getSequelizeConfig,
       inject: [ConfigService],
     }),
+    UsersModule,
+    OrganizationsModule,
+    ProjectsModule,
+    TasksModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
