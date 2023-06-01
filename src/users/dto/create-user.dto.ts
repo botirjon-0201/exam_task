4aa696969bee3f0d351b375cf960dbfaac656bbc
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-01 17:53:42 +0500

    users module, controller, service, model and dto created and combined

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'ali', description: 'Name of User' })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: 'ali@gmail.com', description: 'User Email' })
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'P@$$w0rd', description: 'User Password' })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @IsStrongPassword()
+  password: string;
+
+  @ApiProperty({ example: 'P@$$w0rd', description: 'Confirm Password' })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  @IsStrongPassword()
+  confirm_password: string;
+
+  @ApiProperty({ example: 'ADMIN', description: 'Role of User' })
+  @IsNotEmpty()
+  @IsString()
+  role: string;
+}
---
+++ src/users/models/user.model.ts
@@ -0,0 +1,63 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import { OrganizationUser } from '../../organizations/models/organization-user.model';
+import { Project } from '../../projects/models/project.model';
+import { Task } from '../../tasks/models/task.model';
+import { Organization } from '../../organizations/models/organization.model';
+
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  HEAD = 'HEAD',
+  EMPLOYEE = 'EMPLOYEE',
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User> {
+  @ApiProperty({ example: 1, description: 'Unique ID' })
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'ali', description: 'User Name' })
+  @Column({ type: DataType.STRING(255), allowNull: false })
+  name: string;
+
+  @ApiProperty({ example: 'ali@gmail.com', description: 'User Email' })
+  @Column({ type: DataType.STRING(255), allowNull: false })
+  email: string;
+
+  @ApiProperty({ example: 'P@$$w0rd', description: 'User Password' })
+  @Column({ type: DataType.STRING(255), allowNull: false })
+  password: string;
+
+  @ApiProperty({ example: 'ADMIN', description: 'User Role' })
+  @Column({ type: DataType.STRING(255), allowNull: false })
+  role: UserRole;
+
+  @ApiProperty({ example: 'refresh token', description: 'Refresh token' })
+  @Column({ type: DataType.STRING(255), allowNull: true })
+  refresh_token: string;
+
+  @ApiProperty({ example: 'admin', description: 'Who created it?' })
+  @Column({ type: DataType.STRING(255), allowNull: true })
+  created_by: number;
+
+  @HasMany(() => Organization, 'created_by')
+  created_organizations: Organization[];
+
+  @HasMany(() => OrganizationUser, 'user_id')
+  organization_users: OrganizationUser[];
+
+  @HasMany(() => Project, 'created_by')
+  created_projects: Project[];
+
+  @HasMany(() => Task, 'created_by')
+  created_tasks: Task[];
+
+  @HasMany(() => Task, 'worker_user_id')
+  tasks: Task[];
+}
