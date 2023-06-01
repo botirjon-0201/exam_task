20911a630025300df3b50ae5fd455b0e1ac04b79
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-01 16:04:31 +0500

    app.module.ts file configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { getFilePathConfig } from './config/filepath.config';
+import { getSequelizeConfig } from './config/sequelize.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(getFilePathConfig()),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getSequelizeConfig,
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
