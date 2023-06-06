3b3da9381602ce7b6e21ac0e94e6ce65c93b43ba
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-06 09:18:48 +0500

    decarotor has been async

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User, UserTypeData } from '../users/models/user.model';
+import { UserTypeData } from '../users/models/user.model';
 
 export const GetUser = createParamDecorator(
-  (data: UserTypeData, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<{ user: User }>();
+  async (data: UserTypeData, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
     const user = request.user;
     return data ? user?.[data] : user;
   },
