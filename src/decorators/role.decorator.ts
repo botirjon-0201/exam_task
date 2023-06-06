2baf0793a2e699f4522eb871deafdc34ab49ae85
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-06 09:16:13 +0500

    employee guard added

--- src/decorators/role.decorator.ts
+++ src/decorators/role.decorator.ts
@@ -3,11 +3,12 @@ import { UserRole } from '../users/models/user.model';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { AdminGuard } from '../guards/admin.guard';
 import { HeadGuard } from '../guards/head.guard';
+import { EmployeeGuard } from '../guards/employee.guard';
 
 export const Role = (role: UserRole = 'EMPLOYEE') => {
   return applyDecorators(
     (role === 'ADMIN' && UseGuards(JwtAuthGuard, AdminGuard)) ||
-      (role === 'EMPLOYEE' && UseGuards(JwtAuthGuard)) ||
+      (role === 'EMPLOYEE' && UseGuards(JwtAuthGuard, EmployeeGuard)) ||
       (role === 'HEAD' && UseGuards(JwtAuthGuard, HeadGuard)),
   );
 };
