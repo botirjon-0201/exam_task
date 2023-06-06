c9b4c87e9ad3ab36fa99f9c7a0ca54735e827acd
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-06-06 09:20:17 +0500

    optimize code

--- src/guards/head.guard.ts
+++ src/guards/head.guard.ts
@@ -5,25 +5,19 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { User } from '../users/models/user.model';
 
 @Injectable()
 export class HeadGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-
-    const user = request.user;
+    const { user, body } = context.switchToHttp().getRequest();
     if (user.role !== 'HEAD')
       throw new ForbiddenException("Sorry you don't have access to the page");
 
-    const dto = request.body;
-    if (Object.keys(dto).length) {
-      if (!user.organizations.some((org) => org.id === dto.org_id))
+    if (Object.keys(body).length && body.org_id)
+      if (!user.organizations.some((org) => org.id === body.org_id))
         throw new ForbiddenException("Sorry you aren't head of organization");
-      return true;
-    } else {
-      return true;
-    }
+
+    return true;
   }
 }
